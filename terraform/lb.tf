09b8272a5f969826150aad9af1bc11a520f1e56b
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-04-25 19:15:10 +0200

    Destroying an LB prior to rebuild

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,9 +1,9 @@
-resource "hcloud_server" "loadbalancer" {
-  name        = "loadbalancer"
-  image       = "debian-12"
-  server_type = "cx11"
-  public_net {
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
-}
+# resource "hcloud_server" "loadbalancer" {
+#   name        = "loadbalancer"
+#   image       = "debian-12"
+#   server_type = "cx11"
+#   public_net {
+#     ipv4_enabled = true
+#     ipv6_enabled = true
+#   }
+# }
