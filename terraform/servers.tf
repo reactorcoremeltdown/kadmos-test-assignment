52047b5c6dbdf88d8dc671aebb91b6843d9ef1eb
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-04-26 16:40:39 +0200

    Added kubernetes node

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -10,7 +10,19 @@ resource "hcloud_server" "loadbalancer" {
   }
 }
 
+resource "hcloud_server" "kubernetes" {
+  name        = "kubernetes"
+  image       = "debian-12"
+  server_type = "cx11"
+  ssh_keys = [ hcloud_ssh_key.kadmos.name ]
+  firewall_ids = [ hcloud_firewall.kadmos.id, hcloud_firewall.kubernetes.id ]
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
+
 resource "local_file" "ansible_inventory" {
   filename = "inventory.txt"
-  content = join("\n", ["[loadbalancers]", hcloud_server.loadbalancer.ipv4_address])
+  content = join("\n", ["[loadbalancers]", hcloud_server.loadbalancer.ipv4_address], "", "[kubernetes]", hcloud_server.kubernetes.ipv4_address)
 }
